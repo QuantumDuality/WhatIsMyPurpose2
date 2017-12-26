ded28473c13f47dfe48d4e95ae7c132f2ff66f88
root <012010256@upq.edu.mx>
2017-12-25 18:15:19 -0600

    BT capability added

---
+++ server/server.py
@@ -0,0 +1,42 @@
+'''
+    Simple socket server using threads
+'''
+ 
+import socket
+import sys
+ 
+HOST = '192.168.1.68'   # Symbolic name, meaning all available interfaces
+PORT = 5000 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	while 1:     
+		data = conn.recv(1024) 
+		line = data.decode('UTF-8')
+
+		if data != "":
+			print data
+		else:
+			break
+ 
+	
+	     
